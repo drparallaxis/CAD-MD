414cf5da0837df11850b49796a05c4d1d8cbfa10
Alan Kokaev <44215959+drparallaxis@users.noreply.github.com>
2022-03-09 15:15:39 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>
+#include <math.h>
+#include "Processor.h"
+using namespace std;
+
+int main()
+{
+	Processor Object;
+	Object.start();
+	_getch();
+}
